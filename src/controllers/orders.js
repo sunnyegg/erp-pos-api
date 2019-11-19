4fd9f47701142ca2e32cde878aa3ae00039d8d27
Adila <adilatresna21@gmail.com>
2019-11-19 12:27:40 +0700

    Order [OK], Checkout [OK]

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,10 +1,12 @@
 // Import
 const orderModel = require("../models/orders");
+const schema = require("../configs/validation");
 
 // Controllers
 module.exports = {
   getOrders: (req, res) => {
-    orderModel.getOrders
+    orderModel
+      .getOrders()
       .then(orders => {
         if (!orders.length) {
           res.status(404).json({
@@ -29,71 +31,92 @@ module.exports = {
   },
   addNewOrder: (req, res) => {
     const order = {
-      order_table: req.body,
-      order_menu_id: req.body,
-      order_quantity: req.body,
-      order_added_by: req.body
+      order_table: req.body.order_table,
+      order_menu_id: req.body.order_menu_id,
+      order_description: req.body.order_description,
+      order_quantity: req.body.order_quantity,
+      order_added_by: req.body.order_added_by,
+      customer_id: req.body.customer_id
     };
 
-    orderModel
-      .addNewOrder(order)
-      .then(() => {
-        res.status(200).json({
-          status: 200,
-          message: "Order added successfully!",
-          data: order
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          status: 500,
-          message: "Order failed!"
-        });
+    const validation = schema.order_add.validate(order);
+
+    if (validation.error) {
+      res.status(400).json({
+        status: 400,
+        message: validation.error.details[0].message
       });
-  },
-  editOrder: (req, res) => {
-    const id = parseInt(req.params);
-    const edit = {
-      order_table: req.body,
-      order_menu_id: req.body,
-      order_quantity: req.body,
-      order_added_by: req.body
-    };
-
-    orderModel
-      .editOrder(edit, id)
-      .then(order => {
-        if (!order.affectedRows) {
-          res.status(404).json({
-            status: 404,
-            message: "There is no order with ID: " + id
-          });
-        } else {
+    } else {
+      orderModel
+        .addNewOrder(order)
+        .then(() => {
           res.status(200).json({
             status: 200,
-            message: "Order edited successfully!",
-            data: edit
+            message: "Order added successfully!",
+            data: order
           });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          status: 500,
-          message: "Edit failed!"
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            status: 500,
+            message: "Order failed! " + err
+          });
         });
+    }
+  },
+  editOrder: (req, res) => {
+    const id = parseInt(req.params.id);
+    const edit = {
+      order_table: req.body.order_table,
+      order_menu_id: req.body.order_menu_id,
+      order_description: req.body.order_description,
+      order_quantity: req.body.order_quantity,
+      customer_id: req.body.customer_id
+    };
+
+    const validation = schema.order_edit.validate(edit);
+
+    if (validation.error) {
+      res.status(400).json({
+        status: 400,
+        message: validation.error.details[0].message
       });
+    } else {
+      orderModel
+        .editOrder(edit, id)
+        .then(order => {
+          if (!order.affectedRows) {
+            res.status(404).json({
+              status: 404,
+              message: "There is no order with ID: " + id
+            });
+          } else {
+            res.status(200).json({
+              status: 200,
+              message: "Order edited successfully!",
+              data: edit
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            status: 500,
+            message: "Edit failed!"
+          });
+        });
+    }
   },
   updateStatusOrder: (req, res) => {
-    const id = parseInt(req.params);
+    const id = parseInt(req.params.id);
     const update = {
-      order_menu_status: req.body,
-      order_updated_by: req.body
+      order_menu_status: req.body.order_menu_status,
+      order_updated_by: req.body.order_updated_by
     };
 
     orderModel
-      .updateOrder(update, id)
+      .updateStatusOrder(update, id)
       .then(order => {
         if (!order.affectedRows) {
           res.status(404).json({
@@ -112,12 +135,12 @@ module.exports = {
         console.log(err);
         res.status(500).json({
           status: 500,
-          message: "Update failed!"
+          message: "Update failed! " + err
         });
       });
   },
   deleteOrder: (req, res) => {
-    const id = parseInt(req.params);
+    const id = parseInt(req.params.id);
 
     orderModel
       .deleteOrder(id)
--- src/models/orders.js
+++ src/models/orders.js
@@ -6,7 +6,7 @@ module.exports = {
   getOrders: () => {
     return new Promise((resolve, reject) => {
       conn.query(
-        "SELECT orders.order_id, orders.order_invoice, orders.order_table, menu.menu_name, menu_category.category_name, menu.menu_price, orders.order_quantity, order_menu_added, orders_status.status_name FROM orders INNER JOIN menu ON orders.order_menu_id = menu.menu_id INNER JOIN menu_category ON menu.menu_category = menu_category.category_id INNER JOIN orders_status ON orders.order_menu_status = orders_status.status_id",
+        "SELECT orders.order_id, customers.customer_name, orders.order_invoice, orders.order_table, menu.menu_name, menu_category.category_name, menu.menu_price, orders.order_description, orders.order_quantity, order_menu_added, orders_status.status_name AS status FROM orders INNER JOIN menu ON orders.order_menu_id = menu.menu_id INNER JOIN menu_category ON menu.menu_category = menu_category.category_id INNER JOIN orders_status ON orders.order_menu_status = orders_status.status_id INNER JOIN customers ON orders.customer_id = customers.customer_id",
         (err, result) => {
           if (!err) {
             resolve(result);
@@ -21,7 +21,34 @@ module.exports = {
     return new Promise((resolve, reject) => {
       conn.query("INSERT INTO orders SET ?", data, (err, result) => {
         if (!err) {
-          resolve(result);
+          conn.query(
+            "SELECT menu_quantity FROM menu WHERE menu_id = ?",
+            data.order_menu_id,
+            (err, update) => {
+              const countQuantity = update[0].menu_quantity - data.order_quantity;
+
+              if (countQuantity < 1) {
+                reject("Quantity cannot below 0");
+              } else {
+                const date = new Date();
+                const format =
+                  "ERP" +
+                  date.getDate().toString() +
+                  date.getMonth() +
+                  date.getFullYear() +
+                  "-" +
+                  data.customer_id;
+
+                conn.query(
+                  `UPDATE orders SET order_invoice = '${format}' WHERE order_id = ${result.insertId}`
+                );
+                resolve(result);
+              }
+              if (err) {
+                reject(err);
+              }
+            }
+          );
         } else {
           reject(new Error(err));
         }
@@ -32,7 +59,32 @@ module.exports = {
     return new Promise((resolve, reject) => {
       conn.query("UPDATE orders SET ? WHERE order_id = ?", [data, id], (err, result) => {
         if (!err) {
-          resolve(result);
+          conn.query(
+            "SELECT menu_quantity FROM menu WHERE menu_id = ?",
+            data.order_menu_id,
+            (err, update) => {
+              const countQuantity = update[0].menu_quantity - data.order_quantity;
+
+              if (countQuantity < 1) {
+                reject("Quantity cannot below 0");
+              } else {
+                const date = new Date();
+                const format =
+                  "ERP" +
+                  date.getDate().toString() +
+                  date.getMonth() +
+                  date.getFullYear() +
+                  "-" +
+                  data.customer_id;
+
+                conn.query(`UPDATE orders SET order_invoice = '${format}' WHERE order_id = ${id}`);
+                resolve(result);
+              }
+              if (err) {
+                reject(err);
+              }
+            }
+          );
         } else {
           reject(new Error(err));
         }
@@ -44,20 +96,62 @@ module.exports = {
       conn.query(
         "SELECT user_id, user_type FROM users WHERE user_id = ?",
         data.order_updated_by,
-        (err, result) => {
-          if (result[0].user_type !== 1) {
-            reject("You are not allowed to do that.");
+        (err, user) => {
+          if (!user.length) {
+            reject("There is no user with ID: " + data.order_updated_by);
           } else {
-            if (!err) {
-              conn.query("UPDATE orders SET ? WHERE order_id = ?", [data, id], (err, result) => {
-                if (!err) {
-                  resolve(result);
-                } else {
-                  reject(new Error(err));
-                }
-              });
+            if (user[0].user_type !== 1) {
+              reject("You are not allowed to do that.");
             } else {
-              reject(err);
+              if (!err) {
+                conn.query(
+                  "SELECT order_menu_id, order_quantity FROM orders WHERE order_id = ?",
+                  id,
+                  (err, order) => {
+                    if (!err) {
+                      conn.query(
+                        "SELECT menu_quantity FROM menu WHERE menu_id = ?",
+                        order[0].order_menu_id,
+                        (err, quantity) => {
+                          const countQuantity = quantity[0].menu_quantity - order[0].order_quantity;
+
+                          if (countQuantity < 1) {
+                            conn.query(
+                              "UPDATE menu SET menu_status = 1 WHERE menu_id = ?",
+                              order[0].order_menu_id
+                            );
+                          }
+
+                          if (!err) {
+                            conn.query("UPDATE menu SET menu_quantity = ? WHERE menu_id = ?", [
+                              countQuantity,
+                              order[0].order_menu_id
+                            ]);
+
+                            conn.query(
+                              "UPDATE orders SET ? WHERE order_id = ?",
+                              [data, id],
+                              (err, result) => {
+                                if (!err) {
+                                  resolve(result);
+                                } else {
+                                  reject(new Error(err));
+                                }
+                              }
+                            );
+                          } else {
+                            reject(err);
+                          }
+                        }
+                      );
+                    } else {
+                      reject(err);
+                    }
+                  }
+                );
+              } else {
+                reject(err);
+              }
             }
           }
         }
